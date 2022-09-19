5f27ed278c0c963c0f4d9995ea0cec35abba1070
Fabian Silva <84555439+fabiansilva2@users.noreply.github.com>
2022-09-19 15:17:44 -0400

    PrimeiroCommit

---
+++ src/main/java/com/example/testecentecprojetos/TestecentecprojetosApplication.java
@@ -0,0 +1,13 @@
+package com.example.testecentecprojetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestecentecprojetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestecentecprojetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testecentecprojetos/TestecentecprojetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testecentecprojetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestecentecprojetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
